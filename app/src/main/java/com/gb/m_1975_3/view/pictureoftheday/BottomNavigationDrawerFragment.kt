fa707029bd776da39880c215971842361c14b881
Andej Nesterenko <mentatus.n@gmail.com>
2022-06-07 23:23:10 +0300

    урок 1 | BottomAppBar+FAB BottomNavigationDrawerFragment.kt

---
+++ app/src/main/java/com/gb/m_1975_3/view/pictureoftheday/BottomNavigationDrawerFragment.kt
@@ -0,0 +1,44 @@
+package com.gb.m_1975_3.view.pictureoftheday
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.gb.m_1975_3.R
+import com.gb.m_1975_3.databinding.BottomNavigationLayoutBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
+
+
+    private var _binding: BottomNavigationLayoutBinding? = null
+    private val binding: BottomNavigationLayoutBinding
+        get(){
+            return _binding!!
+        }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = BottomNavigationLayoutBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navigation_one -> Toast.makeText(context, "1", Toast.LENGTH_SHORT).show()
+                R.id.navigation_two -> Toast.makeText(context, "2", Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+    }
+}
--- app/src/main/java/com/gb/m_1975_3/view/pictureoftheday/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/gb/m_1975_3/view/pictureoftheday/PictureOfTheDayFragment.kt
@@ -4,23 +4,27 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import coil.load
 import com.gb.m_1975_3.R
 import com.gb.m_1975_3.databinding.FragmentPictureOfTheDayBinding
+import com.gb.m_1975_3.view.MainActivity
 import com.gb.m_1975_3.viewmodel.AppState
 import com.gb.m_1975_3.viewmodel.PictureOfTheDayViewModel
+import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import java.util.*
 
 
 class PictureOfTheDayFragment : Fragment() {
 
+
+    var isMain = true
 
     private var _binding: FragmentPictureOfTheDayBinding? = null
     private val binding: FragmentPictureOfTheDayBinding
@@ -40,8 +44,27 @@ class PictureOfTheDayFragment : Fragment() {
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_bottom_bar, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.app_bar_fav -> Toast.makeText(context, "Favourite", Toast.LENGTH_SHORT).show()
+            R.id.app_bar_settings -> Toast.makeText(context, "Settings", Toast.LENGTH_SHORT).show()
+            android.R.id.home->{
+                BottomNavigationDrawerFragment().show(requireActivity().supportFragmentManager,"")
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+        (requireActivity() as MainActivity).setSupportActionBar(binding.bottomAppBar)
         viewModel.getLiveDataForViewToObserve().observe(viewLifecycleOwner) {
             renderData(it)
         }
@@ -76,6 +99,22 @@ class PictureOfTheDayFragment : Fragment() {
             }
 
         })
+
+        binding.fab.setOnClickListener {
+            isMain = !isMain
+            if (!isMain) {
+                binding.bottomAppBar.navigationIcon = null
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+                binding.fab.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_back_fab))
+                binding.bottomAppBar.replaceMenu(R.menu.menu_bottom_bar_other)
+            } else {
+                binding.bottomAppBar.navigationIcon =
+                    ContextCompat.getDrawable(requireContext(), R.drawable.ic_hamburger_menu_bottom_bar)
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+                binding.fab.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_plus_fab))
+                binding.bottomAppBar.replaceMenu(R.menu.menu_bottom_bar)
+            }
+        }
 
     }
 
