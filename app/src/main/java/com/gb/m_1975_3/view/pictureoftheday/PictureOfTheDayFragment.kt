7f9e1d899da1015730b29b46e4effaa416351abe
Andej Nesterenko <mentatus.n@gmail.com>
2022-06-07 22:53:18 +0300

    урок 1 | BottomSheetBehavior

--- app/src/main/java/com/gb/m_1975_3/view/pictureoftheday/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/gb/m_1975_3/view/pictureoftheday/PictureOfTheDayFragment.kt
@@ -3,16 +3,19 @@ package com.gb.m_1975_3.view.pictureoftheday
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.lifecycle.ViewModelProvider
 import coil.load
 import com.gb.m_1975_3.R
 import com.gb.m_1975_3.databinding.FragmentPictureOfTheDayBinding
 import com.gb.m_1975_3.viewmodel.AppState
 import com.gb.m_1975_3.viewmodel.PictureOfTheDayViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import java.util.*
 
 
@@ -44,11 +47,36 @@ class PictureOfTheDayFragment : Fragment() {
         }
         viewModel.sendServerRequest()
 
-        binding.inputLayout.setEndIconOnClickListener{
+        binding.inputLayout.setEndIconOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW).apply {
-                data = Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
+                data =
+                    Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
             })
         }
+
+        val params =
+            (binding.lifeHack.bottomSheetContainer.layoutParams as CoordinatorLayout.LayoutParams)
+        val behavior = params.behavior as BottomSheetBehavior
+        behavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+        behavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                 when(newState){
+                  /*   BottomSheetBehavior.STATE_DRAGGING -> TODO("not implemented")
+                     BottomSheetBehavior.STATE_COLLAPSED -> TODO("not implemented")
+                     BottomSheetBehavior.STATE_EXPANDED -> TODO("not implemented")
+                     BottomSheetBehavior.STATE_HALF_EXPANDED -> TODO("not implemented")
+                     BottomSheetBehavior.STATE_HIDDEN -> TODO("not implemented")
+                     BottomSheetBehavior.STATE_SETTLING -> TODO("not implemented")*/
+                 }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                Log.d("@@@","$slideOffset slideOffset")
+            }
+
+        })
+
     }
 
     private fun renderData(appState: AppState) {
@@ -62,8 +90,10 @@ class PictureOfTheDayFragment : Fragment() {
                 appState.serverResponseData.hdurl =
                     "https://api.nasa.gov/assets/img/favicons/favicon-192.png"
                 binding.imageView.load(appState.serverResponseData.hdurl) {
-                    // TODO placehilde+error+transform
+                    // TODO HW placehilde+error+transform
                 }
+                binding.lifeHack.title.text =   appState.serverResponseData.title
+                // TODO HW appState.serverResponseData.explanation
             }
         }
     }
