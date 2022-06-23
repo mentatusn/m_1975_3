cf3e967c3f59a04feb50cb8aa127d390a20b458a
Andej Nesterenko <mentatus.n@gmail.com>
2022-06-23 22:26:37 +0300

    Урок 5 | ConstraintSet 1 способ

--- app/src/main/java/com/gb/m_1975_3/view/animations/AnimationsActivity.kt
+++ app/src/main/java/com/gb/m_1975_3/view/animations/AnimationsActivity.kt
@@ -1,28 +1,42 @@
 package com.gb.m_1975_3.view.animations
 
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.ObjectAnimator
 import android.os.Bundle
-import android.view.View
+import androidx.transition.TransitionManager
+import android.view.animation.AnticipateOvershootInterpolator
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat.animate
-import com.gb.m_1975_3.databinding.ActivityAnimationsBinding
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.transition.ChangeBounds
+import com.gb.m_1975_3.R
+import com.gb.m_1975_3.databinding.ActivityAnimationsBonusStartBinding
 
 class AnimationsActivity : AppCompatActivity() {
-    lateinit var binding: ActivityAnimationsBinding
+    lateinit var binding: ActivityAnimationsBonusStartBinding
     private var flag = false
     private var duration = 1000L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAnimationsBinding.inflate(layoutInflater)
+        binding = ActivityAnimationsBonusStartBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
-        binding.scrollView.setOnScrollChangeListener { _, _, _, _, _ ->
-            binding.header.isSelected =binding.scrollView.canScrollVertically(-1)
+        val constraintSetStart = ConstraintSet()
+        val constraintSetEnd= ConstraintSet()
+        constraintSetStart.clone(binding.root)
+        constraintSetEnd.load(this, R.layout.activity_animations_bonus_end)
+        binding.backgroundImage.setOnClickListener {
+            flag = !flag
+            val changeBounds = ChangeBounds()
+            changeBounds.duration =1000L
+            changeBounds.interpolator = AnticipateOvershootInterpolator(5.0f)
+            TransitionManager.beginDelayedTransition(binding.root,changeBounds)
+            if(flag){
+                constraintSetEnd.applyTo(binding.root)
+            }else{
+                constraintSetStart.applyTo(binding.root)
+            }
         }
+
     }
 
 
