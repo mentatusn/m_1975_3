6be93711ea7747866426418c9083440f09ce2d3a
Andej Nesterenko <mentatus.n@gmail.com>
2022-06-23 20:33:07 +0300

    Урок 5 | познакомились с TransitionManager

--- app/src/main/java/com/gb/m_1975_3/view/animations/AnimationsActivity.kt
+++ app/src/main/java/com/gb/m_1975_3/view/animations/AnimationsActivity.kt
@@ -2,15 +2,13 @@ package com.gb.m_1975_3.view.animations
 
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.transition.ChangeBounds
+import android.transition.Fade
+import android.transition.TransitionManager
+import android.transition.TransitionSet
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.gb.m_1975_3.R
 import com.gb.m_1975_3.databinding.ActivityAnimationsBinding
-import com.gb.m_1975_3.databinding.ActivityApiBinding
-import com.gb.m_1975_3.view.api.BaseFragment
-import com.gb.m_1975_3.view.api.ViewPager2Adapter
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
 
 class AnimationsActivity : AppCompatActivity() {
     lateinit var binding: ActivityAnimationsBinding
@@ -21,25 +19,36 @@ class AnimationsActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.button.setOnClickListener {
+
+            val myAutoTransition = TransitionSet()
+            myAutoTransition.ordering = TransitionSet.ORDERING_SEQUENTIAL // по очереди
+            myAutoTransition.addTransition(Fade(Fade.OUT))
+            myAutoTransition.addTransition(ChangeBounds())
+            myAutoTransition.addTransition(Fade(Fade.IN))
+            myAutoTransition.duration = 2000L
+            TransitionManager.beginDelayedTransition(binding.root,myAutoTransition)
             flag = !flag
             if (flag) {
                 binding.text.visibility = View.VISIBLE
             } else {
-                //binding.text.visibility = View.GONE
-                val timer = object : CountDownTimer(2000L,200L){
-                    override fun onTick(millisUntilFinished: Long) {
-                        binding.text.alpha -= 0.1f
-                        binding.text.scaleY -=0.1f
-                    }
-                    override fun onFinish() {
-                        binding.text.alpha = 0f
-                        binding.text.scaleY = 0f
-                        binding.text.visibility = View.GONE
-                    }
-                }
-                timer.start()
+                binding.text.visibility = View.GONE
             }
         }
 
+    }
+
+    fun old(){
+        val timer = object : CountDownTimer(2000L,200L){
+            override fun onTick(millisUntilFinished: Long) {
+                binding.text.alpha -= 0.1f
+                binding.text.scaleY -=0.1f
+            }
+            override fun onFinish() {
+                binding.text.alpha = 0f
+                binding.text.scaleY = 0f
+                binding.text.visibility = View.GONE
+            }
+        }
+        timer.start()
     }
 }
